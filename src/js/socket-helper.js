25e5c4616d8dcf12c287022be523a6cf162419ae
Kevin Wad <k.w.x.m.r@protonmail.com>
2023-04-16 15:22:36 +0200

    Try fix websocket issue, only create new websocket connectioon if not already an existing websocket connection

--- src/js/socket-helper.js
+++ src/js/socket-helper.js
@@ -6,41 +6,37 @@ var io = require('socket.io-client');
 var socket;
 
 export default class SocketHelper{
+  constructor() {
+    this.socket = null;
+  }
 
-
-    initSocket(){
-
-        var current_hostname = window.location.hostname;
-
-        const socketServer = 'http://'+current_hostname+':2052';
-  
-        const options = {transports: ['websocket'], forceNew: true};
-        socket = io(socketServer, options);
-  
-  
-        // Socket events
-        socket.on('connect', () => {
-          console.log('connected to socket.io server');
-        });
-  
-  
-        socket.on('disconnect', () => {
-          console.log('disconnected from socket.io server');
-        });
-   
-
-      
-       return socket    
-
-      //  setInterval(this.update.bind(this),5000);
+  initSocket() {
+    if (this.socket) {
+      // Return the existing socket instance if it has already been created
+      return this.socket;
     }
+
+    var current_hostname = window.location.hostname;
+    const socketServer = 'http://' + current_hostname + ':2052';
+    const options = { transports: ['websocket'], forceNew: true };
+    this.socket = io(socketServer, options);
+
+    // Socket events
+    this.socket.on('connect', () => {
+      console.log('connected to socket.io server');
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('disconnected from socket.io server');
+    });
+
+    return this.socket;
+  }
 
 
     emitEvent(evt,args){
         console.log('emit',evt,args)
-        socket.emit(evt,args); 
-       
-         
+        this.socket.emit(evt,args);   
     }
  
 
