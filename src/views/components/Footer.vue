643f9bd6d32551c60d084b46d750680189540add
Kevin Wad <k.w.x.m.r@protonmail.com>
2023-11-23 18:35:01 +0100

    Updates homepage

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -9,7 +9,9 @@
           <a href="https://www.eticaprotocol.org/viewwhitepaper" target="_blank" class="footer-link">White Paper</a>
           <a href="https://www.eticaprotocol.org/eticadocs/mining.html" target="_blank" class="footer-link">How to mine Etica</a>
           <a href="https://www.eticaprotocol.org/eticadocs/howitworks.html" target="_blank" class="footer-link">How it works</a>
-          <a href="https://txbit.io/Trade/ETI/USDT" target="_blank" class="footer-link">Exchanges</a>
+          <a href="https://www.eticaprotocol.org/exchanges" target="_blank" class="footer-link">Exchanges</a>
+          <a href="https://www.youtube.com/watch?v=IaIDSLBxzjg" target="_blank" class="footer-link">Create Etica Wallet (Metamask)</a>
+          <a href="https://github.com/etica/etica-gui/releases/tag/v1.0.7" target="_blank" class="footer-link">Create Etica Wallet (GUI)</a>
       
         </div>
         <div class="column-6 w-col w-col-4 w-col-small-4">
@@ -20,17 +22,16 @@
           <a href="https://t.me/eticaprotocol" target="_blank" class="footer-link">Telegram</a>
           </div>
         <div class="column-7 w-col w-col-4 w-col-small-4 flex flex-col h-full">
-
-          <div class="flex-grow h-32">
-
+        <h3 class="heading-3 footer">Explorers</h3>
+        <a href="https://www.eticascan.org" target="_blank" class="footer-link">https://www.eticascan.org</a>
+        <a href="http://explorer.etica-stats.org/" target="_blank" class="footer-link">http://explorer.etica-stats.org</a>
+        <div class="flex-grow h-32">
          <a href="https://miningpoolstats.stream/etica" target="_blank" class="brand w-nav-brand w--current">
                <img src="@/assets/images/egaz-logo.png" height="100"  alt="" class="w-6 m-2">
             <div class=" inline-block text-xl text-white-100" style="position: relative;top: 0.75vh;color: white;">Egaz pools</div>
          </a>
-
-          </div>
-
-         
+         <a href="https://www.youtube.com/watch?v=tPljgu0rez0" target="_blank" class="footer-link">How to mine EGAZ</a>
+        </div>
 
          </div>
       </div>
