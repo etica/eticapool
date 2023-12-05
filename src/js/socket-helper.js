60071cd93063e848ab4609dd267dbadde9a2b248
Kevin Wad <k.w.x.m.r@protonmail.com>
2023-12-05 17:19:58 +0100

    Eticapool release v1.0.9. Implements Ppnls reward system.

--- src/js/socket-helper.js
+++ src/js/socket-helper.js
@@ -17,7 +17,7 @@ export default class SocketHelper{
     }
 
     var current_hostname = window.location.hostname;
-    const socketServer = 'http://' + current_hostname + ':2052';
+    const socketServer = 'http://' + current_hostname + ':2053';
     const options = { transports: ['websocket'], forceNew: true };
     this.socket = io(socketServer, options);
 
