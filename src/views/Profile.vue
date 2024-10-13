1dc0cdb40cb41dd0638a312bce08bb327deb9bdc
Kevin Wad <k.w.x.m.r@protonmail.com>
2024-10-13 20:45:22 +0200

    Eticapool Release v 4.0.14, impelemnts workers data on main address page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -229,6 +229,8 @@
       <div v-if="activeSection=='Workers'"  class="box  background-secondary overflow-x-auto" 
          style="  min-height:480px;">
 
+        <span class='text-lg font-bold'>{{ minerData.workers.length }} workers</span> 
+
         <div class='text-lg font-bold' style="color: #868686;">This is a new feature, restart workers to see whole metrics</div>
         <table class='table w-full'>
 
